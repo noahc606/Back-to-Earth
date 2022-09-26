0d5fee23ea694a21693cd192b187abb0035492da
noahc606 <noah.c.h@outlook.com>
2022-09-25 21:30:37 -0400

    Testing testing testing testing testing testing... TESTING AAAAA

--- src/main/Tests.cpp
+++ src/main/Tests.cpp
@@ -3,6 +3,7 @@
 #include "Text.h"
 #include "Window.h"
 #include "Real.h"
+#include "Noise.h"
 /**/
 
 Tests::Tests(){}
@@ -10,12 +11,31 @@ void Tests::init(SDLHandler* sh, FileHandler* fh, Controls* ctrls)
 {
     BTEObject::init(sh, fh, ctrls);
 
-    Real x = -5.7;
-    Real y =  3.1;
+    //SpriteSheet ss;
+    //ColorPalette cpal; cpal.init(cpal.DEFAULT_PLAYER);
 
-    std::cout << "Test:\n";
-    std::cout << (x+y).getString() << "\n";
-    std::cout << "Test end\n";
+    //SpriteSheetBuilder ssb(sh);
+
+    //ssb.buildSpriteSheet(ssb.DEFAULT_PLAYER, spsh, playerPal);
+
+    tex.init(sdlHandler);
+    tex.setTexDimensions(514, 514);
+
+    Noise noise;
+    tex.rect(0, 0, 514, 514, 255, 0, 0);
+
+    float zoom = 256.0;
+    float tx = 0.0;
+    float ty = 0.0;
+
+    for( int x = 0; x<512; x++ ) {
+        for( int y = 0; y<512; y++ ) {
+            float n = noise.noise2D((float)x/zoom+tx, (float)y/zoom+ty);
+            int c = (n*0.5d+0.5d)*256.0;
+            tex.px(x+1, y+1, c, c, c );
+        }
+    }
+
 
 }
 Tests::~Tests(){}
@@ -24,6 +44,7 @@ Tests::~Tests(){}
 
 void Tests::draw()
 {
+    tex.draw();
 }
 
 void Tests::tick()
