5fd617752004b2952a84cf1cb9585fce202d41ac
Noah Hebert <noah.c.h@outlook.com>
2024-05-05 17:13:05 -0400

    Separate Items and PlayerMenu (more to be done later?)

--- src/world/player/PlayerMenu.cpp
+++ src/world/player/PlayerMenu.cpp
@@ -8,29 +8,30 @@ void PlayerMenu::init(SDLHandler* sh, GUIHandler* gh, Controls* ctrls, Player* p
 	sdlHandler = sh;
 	guiHandler = gh;
 	controls = ctrls;
-	player = pl;
-	
+
+	items.init(guiHandler, pl);
+
 	for( int x = 0; x<invW; x++ ) {
 		for( int y = 0; y<invH; y++ ) {
 			inventorySlots[x][y] = -1; 
 		}
 	}
 	
-	inventorySlots[3][0] = ION_BLASTER;
-	inventorySlots[4][1] = GEOPORTER;
-	inventorySlots[6][2] = SOLID_PLASMA_ANNIHILATOR;
-	inventorySlots[6][3] = ATOM_PRINTER;
-	inventorySlots[7][0] = NYLON_EXOSUIT_HELMET;
-	inventorySlots[7][1] = NYLON_EXOSUIT_BODY;
-	inventorySlots[7][2] = NYLON_EXOSUIT_LEGGINGS;
+	inventorySlots[3][0] = Items::ION_BLASTER;
+	inventorySlots[4][1] = Items::GEOPORTER;
+	inventorySlots[6][2] = Items::SOLID_PLASMA_ANNIHILATOR;
+	inventorySlots[6][3] = Items::ATOM_PRINTER;
+	inventorySlots[7][0] = Items::NYLON_EXOSUIT_HELMET;
+	inventorySlots[7][1] = Items::NYLON_EXOSUIT_BODY;
+	inventorySlots[7][2] = Items::NYLON_EXOSUIT_LEGGINGS;
 	
-	inventorySlots[7][7] = FOOD_RATION_F;
-	inventorySlots[7][6] = FOOD_RATION_F;
-	inventorySlots[7][5] = FOOD_RATION_A;
-	inventorySlots[7][4] = FOOD_RATION_B;
-	inventorySlots[7][3] = FOOD_RATION_C;
-	inventorySlots[6][7] = FOOD_RATION_D;
-	inventorySlots[6][6] = FOOD_RATION_E;
+	inventorySlots[7][7] = Items::FOOD_RATION_F;
+	inventorySlots[7][6] = Items::FOOD_RATION_F;
+	inventorySlots[7][5] = Items::FOOD_RATION_A;
+	inventorySlots[7][4] = Items::FOOD_RATION_B;
+	inventorySlots[7][3] = Items::FOOD_RATION_C;
+	inventorySlots[6][7] = Items::FOOD_RATION_D;
+	inventorySlots[6][6] = Items::FOOD_RATION_E;
 	uiOverlay.init(sdlHandler, 0, 0);
 	
 	movingItemA.init(sdlHandler, 32, 32);
@@ -46,10 +47,10 @@ void PlayerMenu::tick() {
 		lastState = state;
 		
 		if(state==1) {
-			putItemInterface(-1);
+			items.putItemInterface(-1);
 		}
 		if(state==2) {
-			putItemInterface(SANDBOX);
+			items.putItemInterface(Items::SANDBOX);
 		}
 	}
 	
@@ -101,7 +102,7 @@ void PlayerMenu::tick() {
 	
 		//If item selection has changed
 		if( newItemSelected && state==1 ) {
-			putItemInterface( inventorySlots[invSX][invSY] );
+			items.putItemInterface( inventorySlots[invSX][invSY] );
 			newItemSelected = false;
 		}
 	}
@@ -118,11 +119,13 @@ void PlayerMenu::tick() {
 						colorInt = 255;
 					}
 					
+					Color newcolor = items.getSandboxRGB();
 					switch(i) {
-						case 0: sandboxRed = colorInt; break;
-						case 1: sandboxGreen = colorInt; break;
-						case 2: sandboxBlue = colorInt; break;
+						case 0: newcolor.r = colorInt; break;
+						case 1: newcolor.g = colorInt; break;
+						case 2: newcolor.b = colorInt; break;
 					}
+					items.setSandboxRGB(newcolor.r, newcolor.g, newcolor.b);
 					
 				} catch(...) { }
 			}
@@ -167,9 +170,9 @@ void PlayerMenu::draw()
 int PlayerMenu::getState() { return state; }
 uint8_t PlayerMenu::getSandboxTexX() { return sandboxTexX; } 
 uint8_t PlayerMenu::getSandboxTexY() { return sandboxTexY; } 
-uint8_t PlayerMenu::getSandboxTexRed() { return sandboxRed; } 
-uint8_t PlayerMenu::getSandboxTexGreen() { return sandboxGreen; } 
-uint8_t PlayerMenu::getSandboxTexBlue() { return sandboxBlue; } 
+uint8_t PlayerMenu::getSandboxTexRed() { return items.getSandboxRGB().r; } 
+uint8_t PlayerMenu::getSandboxTexGreen() { return items.getSandboxRGB().g; } 
+uint8_t PlayerMenu::getSandboxTexBlue() { return items.getSandboxRGB().b; } 
 
 void PlayerMenu::setState(int newState)
 {
@@ -262,28 +265,6 @@ void PlayerMenu::drawInventory()
 	}
 }
 
-std::string PlayerMenu::getItemName(int itemID)
-{
-	switch(itemID) {
-	case SANDBOX:					return "Tile Selection: Texture & Color";
-	case ION_BLASTER: 				return "Ion Blaster";
-	case GEOPORTER:					return "Geo-porter";
-	case SOLID_PLASMA_ANNIHILATOR:	return "Solid-Plasma Annihilator";
-	case ATOM_PRINTER:				return "Atom Printer";
-	case NYLON_EXOSUIT_HELMET:		return "T1 Nylon Exo-Suit Helmet";
-	case NYLON_EXOSUIT_BODY:		return "T1 Nylon Exo-Suit Body";
-	case NYLON_EXOSUIT_LEGGINGS:	return "T1 Nylon Exo-Suit Leggings";
-	case INFINITE_OXYGEN_TANK:		return "Infinite Oxygen Tank";
-	case FOOD_RATION_A:				return "Nutrient Powder A";
-	case FOOD_RATION_B:				return "Nutrient Powder B";
-	case FOOD_RATION_C:				return "MRE Pack (Eggs, Bacon, Toast)";
-	case FOOD_RATION_D:				return "MRE Pack (Steak and Potatoes)";
-	case FOOD_RATION_E:				return "MRE Pack (Chicken and Rice)";
-	case FOOD_RATION_F:				return "MRE Pack (Vegetable Stew)";
-	default: 						return "";
-	}
-}
-
 void PlayerMenu::updateMenuCoordinates()
 {
 	windowW = 0; windowH = 0;
@@ -361,108 +342,5 @@ void PlayerMenu::selectInventorySlot(int x, int y)
 	} else {
 		invSX = -1;
 		invSY = -1;
-	}
-}
-
-void PlayerMenu::putItemInterfaceDesc(Window* win, int dX, int ttpDY, std::string itemDesc, int rowNum)
-{
-	guiHandler->addGUI( new Tooltip(win, dX+32*00, 524+32*rowNum+ttpDY, itemDesc, GUIHandler::ttp_CHARACTER_item) );
-}
-
-void PlayerMenu::putItemInterfaceTitle(Window* win, int dX, int ttpDY, int itemID)
-{
-	guiHandler->addGUI( new Tooltip(win, dX+32*00, 524+32*0+ttpDY, "\""+ getItemName(itemID) + "\"", GUIHandler::ttp_CHARACTER_item) );
-}
-
-void PlayerMenu::putItemInterface(int itemID)
-{
-	Window* win = guiHandler->getWindow(GUIHandler::win_CHARACTER);
-	if( win==nullptr ) {
-		return;
-	}
-	
-	guiHandler->removeGUIs(GUIHandler::ttp_CHARACTER_item, GUIHandler::tbx_CHARACTER_item);
-
-	int dX = 12;
-	int tbxDY = 26;
-	int ttpDY = tbxDY+8;
-	
-	//Add tooltip for every item type
-	guiHandler->addGUI( new Tooltip(win, dX, 524+4, getItemName(itemID), GUIHandler::ttp_CHARACTER_item) );
-	
-	switch( itemID ) {
-	case SANDBOX: {
-		guiHandler->addGUI( new Tooltip(win, dX+32*00, 524+32*0+ttpDY, "Red (0-255):", GUIHandler::ttp_CHARACTER_item) );
-		TextBox* tbx1 = (TextBox*)guiHandler->addGUI( new TextBox(win, dX+32*06, 524+32*0+tbxDY, 100, TextBox::FREE_NUMBERS_BASIC, GUIHandler::tbx_CHARACTER_item), 2000 );
-		std::stringstream ss1; ss1 << (int)sandboxRed;
-		tbx1->setString( ss1.str() );
-		
-		guiHandler->addGUI( new Tooltip(win, dX+32*00, 524+32*1+ttpDY, "Green (0-255):", GUIHandler::ttp_CHARACTER_item) );
-		TextBox* tbx2 = (TextBox*)guiHandler->addGUI( new TextBox(win, dX+32*06, 524+32*1+tbxDY, 100, TextBox::FREE_NUMBERS_BASIC, GUIHandler::tbx_CHARACTER_item), 2001 );
-		std::stringstream ss2; ss2 << (int)sandboxGreen;
-		tbx2->setString( ss2.str() );
-
-		guiHandler->addGUI( new Tooltip(win, dX+32*00, 524+32*2+ttpDY, "Blue (0-255):", GUIHandler::ttp_CHARACTER_item) );
-		TextBox* tbx3 = (TextBox*)guiHandler->addGUI( new TextBox(win, dX+32*06, 524+32*2+tbxDY, 100, TextBox::FREE_NUMBERS_BASIC, GUIHandler::tbx_CHARACTER_item), 2002 );
-		std::stringstream ss3; ss3 << (int)sandboxBlue;
-		tbx3->setString( ss3.str() );
-	} break;
-	case GEOPORTER: {
-
-
-		guiHandler->addGUI( new Tooltip(win, dX+32*00, 524+32*0+ttpDY, "X-position (meters):", GUIHandler::ttp_CHARACTER_item) );
-		TextBox* tbx1 = (TextBox*)guiHandler->addGUI( new TextBox(win, dX+32*06, 524+32*0+tbxDY, 100, TextBox::FREE_NUMBERS_INTEGERS, GUIHandler::tbx_CHARACTER_item), 1000 );
-		std::stringstream ss1; ss1 << (int)std::get<0>(player->getPos());
-		tbx1->setString( ss1.str() );
-		
-		guiHandler->addGUI( new Tooltip(win, dX+32*00, 524+32*1+ttpDY, "Y-position (meters):", GUIHandler::ttp_CHARACTER_item) );
-		TextBox* tbx2 = (TextBox*)guiHandler->addGUI( new TextBox(win, dX+32*06, 524+32*1+tbxDY, 100, TextBox::FREE_NUMBERS_INTEGERS, GUIHandler::tbx_CHARACTER_item), 1001 );
-		std::stringstream ss2; ss2 << (int)std::get<1>(player->getPos());
-		tbx2->setString( ss2.str() );
-
-		guiHandler->addGUI( new Tooltip(win, dX+32*00, 524+32*2+ttpDY, "Z-position (meters):", GUIHandler::ttp_CHARACTER_item) );
-		TextBox* tbx3 = (TextBox*)guiHandler->addGUI( new TextBox(win, dX+32*06, 524+32*2+tbxDY, 100, TextBox::FREE_NUMBERS_INTEGERS, GUIHandler::tbx_CHARACTER_item), 1002 );
-		std::stringstream ss3; ss3 << (int)std::get<2>(player->getPos());
-		tbx3->setString( ss3.str() );
-
-		guiHandler->addGUI( new Tooltip(win, dX+32*10, 524+32*0+ttpDY, "Planet ID:", GUIHandler::ttp_CHARACTER_item) );
-		TextBox* tbx4 = (TextBox*)guiHandler->addGUI( new TextBox(win, dX+32*13+4, 524+32*0+tbxDY, 350, TextBox::FREE_NUMBERS_BASIC, GUIHandler::tbx_CHARACTER_item), 1003 );
-		
-		
-		guiHandler->addGUI( new Button (win, dX+32*10, 524+32*2+tbxDY, 200, "Teleport", GUIHandler::btn_CHARACTER_item), 1004 );
-	} break;
-	case SOLID_PLASMA_ANNIHILATOR: {
-		putItemInterfaceDesc(win, dX, ttpDY, "Destroy solid matter with Left Click", 1);
-	} break;
-	case ATOM_PRINTER: {
-		putItemInterfaceDesc(win, dX, ttpDY, "Build tiles with Right Click - select the tile type from Engineering", 1);
-	} break;
-
-
-
-	case FOOD_RATION_A: {
-		putItemInterfaceDesc(win, dX, ttpDY, "Servings: 128. Calories: 120.", 1);
-		putItemInterfaceDesc(win, dX, ttpDY, "Provides more nutrients but less calories than normal food.", 2);
-	} break;
-	case FOOD_RATION_B: {
-		putItemInterfaceDesc(win, dX, ttpDY, "Servings: 128. Calories: 120.", 1);
-		putItemInterfaceDesc(win, dX, ttpDY, "Provides more nutrients but less calories than normal food.", 2);
-	} break;
-	case FOOD_RATION_C: {
-		putItemInterfaceDesc(win, dX, ttpDY, "Servings: 12. Calories: 350.", 1);
-		putItemInterfaceDesc(win, dX, ttpDY, "\"I'm tired of eating the same breakfast every day.\"", 2);
-	} break;
-	case FOOD_RATION_D: {
-		putItemInterfaceDesc(win, dX, ttpDY, "Servings: 12. Calories: 410.", 1);
-		putItemInterfaceDesc(win, dX, ttpDY, "\"Actually tastes pretty good.\"", 2);
-	} break;
-	case FOOD_RATION_E: {
-		putItemInterfaceDesc(win, dX, ttpDY, "Servings: 10. Calories: 360.", 1);
-		putItemInterfaceDesc(win, dX, ttpDY, "\"It doesn't look that bad.\"", 2);
-	} break;
-	case FOOD_RATION_F: {
-		putItemInterfaceDesc(win, dX, ttpDY, "Servings: 10. Calories: 210.", 1);
-		putItemInterfaceDesc(win, dX, ttpDY, "\"Looks pretty disgusting, but it'll have to do for now...\"", 2);
-	} break;
 	}
 }
